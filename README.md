094d8ceb34cb70925145c6f56f8825bb86cab74d
MarchD <dubchmar@gmail.com>
2021-02-08 01:48:56 +0200

    added solution

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-# Base layout template with Gulp, SCSS and Stylelint
-1. Create a repo using this template
-1. Replace `<your_account>` and `<repo_name>` with your Github username and the new repo name
-    - [DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+# Warehouse
+ - [DEMO LINK](https://marchd.github.io/landing_page/)
+ - [Layout](https://https://www.figma.com/file/Qa9lc1e2FHZQX1fB8lUful/Test-task-(Copy)?node-id=0%3A1>/)
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,77 +1,73 @@
 'use strict';
 
-function toggleModalWindow() {
-  const requestButtons = document.querySelectorAll('.button-request');
-  const modalWindow = document.querySelector('.modal');
+const requestButtons = document.querySelectorAll('.button-request');
+const modalWindow = document.querySelector('.modal');
 
+function toggleModalWindow() {
   [...requestButtons].forEach(button => button.addEventListener('click', () => {
     document.body.style.overflow = 'hidden';
     modalWindow.classList.toggle('modal__show');
   }));
 
-  document.body.addEventListener('click', () => {
+  document.body.addEventListener('click', (event) => {
     const { target } = event;
+
     if (target.classList.contains('modal')) {
       document.body.style.overflow = 'auto';
       modalWindow.classList.toggle('modal__show');
     }
-  })
+  });
 }
 
-function slider() {
-  const frame = document.querySelector('.slider');
-  const dots = document.querySelector('.slider__dots');
-  const itemOfslider = document.querySelector('.image-1');
+const frame = document.querySelector('.slider');
+const dots = document.querySelector('.slider__dots');
+const itemOfSlider = document.querySelector('.image-1');
 
-  console.log(itemOfslider);
+function slider() {
+  if (window.innerWidth < 1100) {
+    itemOfSlider.style.marginLeft = '0';
+  };
 
   dots.addEventListener('click', (event) => {
     const dotValue = +event.target.value;
-    if(dotValue === 1) {
-      itemOfslider.style.marginLeft = '0';
+
+    if (dotValue === 1) {
+      itemOfSlider.style.marginLeft = '0';
     } else {
-      itemOfslider.style.marginLeft = `-${frame.offsetWidth * (dotValue - 1)}px`;
+      itemOfSlider.style.marginLeft = `-${frame.offsetWidth * (dotValue - 1)}px`;
     }
-  })
+  });
 }
-
-toggleModalWindow();
-slider();
-
 
 const form = document.querySelector('.request__form');
 const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
 const webInput = document.getElementById('web');
-const message = document.getElementById('message');
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
+function validation() {
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-  clearError(nameInput, emailInput, webInput)
-  validation(nameInput, emailInput, webInput);
-})
+    clearError(nameInput, emailInput, webInput);
+    validationInputs(nameInput, emailInput, webInput);
+  });
 
-function validation(...inputs) {
+  function validationInputs(...inputs) {
+    inputs.forEach(input => {
+      if (!input.value) {
+        input.classList.add('inputError');
+      }
+    });
+  }
 
-  inputs.forEach(input => {
-    if(!input.value) {
-      input.classList.add('inputError');
-    }
-  })
-
+  function clearError(...inputs) {
+    inputs.forEach(input => {
+      if (input.value) {
+        input.classList.remove('inputError');
+      }
+    });
+  }
 }
-
-function clearError(...inputs) {
-  inputs.forEach(input => {
-    if(input.value) {
-      input.classList.remove('inputError');
-    }
-  })
-}
-
-
-////////////toggle
 
 const toggle = document.querySelector('.nav__toggle');
 const list = document.querySelector('.nav__list');
@@ -79,13 +75,46 @@ const lines = document.querySelectorAll('.nav__toggle-line');
 const firstLine = document.querySelector('.line--1');
 const secondLine = document.querySelector('.line--2');
 const thirdLine = document.querySelector('.line--3');
-const links = document.querySelectorAll('.nav__link');
+const page = document.querySelector('.page');
 
-toggle.addEventListener('click', () => {
+function toggleItems() {
   toggle.classList.toggle('nav__toggle--active');
   lines.forEach(line => line.classList.toggle('nav__toggle-line--active'));
   firstLine.classList.toggle('line--1--active');
   secondLine.classList.toggle('line--2--active');
   thirdLine.classList.toggle('line--3--active');
   list.classList.toggle('nav__list--open');
-})
+  page.classList.toggle('page__non-scroll');
+}
+
+function toggleMenu() {
+  toggle.addEventListener('click', () => {
+    toggleItems();
+  });
+
+  list.addEventListener('click', (event) => {
+    const { target } = event;
+
+    if (target.tagName === 'A' || 'BUTTON') {
+      toggleItems();
+    }
+  });
+};
+
+toggleModalWindow();
+slider();
+validation();
+toggleMenu();
+window.addEventListener('resize', slider);
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth >= 1100) {
+    toggle.classList.remove('nav__toggle--active');
+    lines.forEach(line => line.classList.remove('nav__toggle-line--active'));
+    firstLine.classList.remove('line--1--active');
+    secondLine.classList.remove('line--2--active');
+    thirdLine.classList.remove('line--3--active');
+    list.classList.remove('nav__list--open');
+    page.classList.remove('page__non-scroll');
+  }
+});
